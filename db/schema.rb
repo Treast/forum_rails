1e696a2eadf6fee22f3dc2c304dea0ea22e7a1ff
Treast <baku_kami@hotmail.fr>
2015-02-14 19:57:58 +0100

    Controller & Topic added to Comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214100751) do
+ActiveRecord::Schema.define(version: 20150214185633) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "subject"
