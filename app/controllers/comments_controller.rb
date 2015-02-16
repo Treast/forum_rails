b69a615aeb866eb0a399dd41cf2f407bb84630c4
Treast <baku_kami@hotmail.fr>
2015-02-16 19:47:01 +0100

    Add Comments Model & Integration on Topic Show View

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.create
+  end
+
+  def create 
+    @topic = Topic.find(params[:topic_id])
+    @comment = @topic.comments.create(comment_params)
+    redirect_to @topic
+  end
+
+  
+
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:response)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do 
+      resources :comments
+  end
+
   root to: 'topics#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
